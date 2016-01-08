15f37f8fbafc71cea24feb12c17f76ba6b0d93d4
JDfromM <jdevalerie@gmail.com>
2016-01-08 10:35:28 -0800

    Further cleaned up code

--- src/distance_matrix.java
+++ src/distance_matrix.java
@@ -2,37 +2,37 @@ public class distance_matrix
 {
 	static double distance_formula(double x1, double y1, double x2, double y2)
 	{
-		double dist;
-		double first_part, second_part;
-		first_part = (x2 - x1) * (x2 - x1);
-		second_part = (y2 - y1) * (y2 - y1);
-		dist = Math.sqrt(first_part + second_part);
+		double x_part, y_part;
 
-		return dist;
+		x_part = (x2 - x1) * (x2 - x1);
+		y_part = (y2 - y1) * (y2 - y1);
+
+		return Math.sqrt(x_part + y_part);
 	}
 
 	public static void main (String[] args)
 	{
-		data_ops TEST = new data_ops();
-		TEST.parse_data();
-
-		int entries = TEST.ll.size();
+		data_ops latlon = new data_ops();
+		int entries;
 		double lat1, lon1, lat2, lon2;
-		double result;
+
+		latlon.parse_data();
+		entries = latlon.ll.size();
 		double[][] distance_matrix = new double[entries][entries];
+
 		for (int x = 0; x < entries; x++)
 		{
 			for (int y = 0; y < entries; y++)
 			{
-				lat1 = TEST.ll.get(x).get(0);
-				lon1 = TEST.ll.get(x).get(1);
-				lat2 = TEST.ll.get(y).get(0);
-				lon2 = TEST.ll.get(y).get(1);
+				lat1 = latlon.ll.get(x).get(0);
+				lon1 = latlon.ll.get(x).get(1);
+				lat2 = latlon.ll.get(y).get(0);
+				lon2 = latlon.ll.get(y).get(1);
 
-				result = distance_formula(lat1, lon1, lat2, lon2);
-				distance_matrix[x][y] = result;
+				distance_matrix[x][y] = distance_formula(lat1, lon1, lat2, lon2);
 			}
 		}
+
 		for (int a = 0; a < entries; a++)
 		{
 			for (int b = 0; b < entries; b++)
