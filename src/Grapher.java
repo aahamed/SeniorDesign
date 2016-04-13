52429cb216fa0d8a279c73d13927c9db66a5030b
josue_osx <jdevalerie@gmail.com>
2016-04-12 17:36:03 -0700

    src/Grapher.java

---
+++ src/Grapher.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Josue Galeas
+ * Last Edit: Apr 8, 2016
+ * Description: Class for graphing data points. TODO: Maybe the MST as well?
+ */
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Grapher extends JFrame
+{
+	public Grapher()
+	{
+
+	}
+}
